a506786b178169833a62ca1865ef4441bf8cb1d9
KamillaLima <kamillalima868@gmail.com>
2024-04-12 13:33:29 -0300

    criando crud de produto e fazendo os relacionamentos entre tabelas

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,15 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,17 +26,28 @@ public class Categoria {
 	private String nome;
 	@Size(min=5 , message="O TAMANHO MINIMO DA DESCRICAO PARA A CATEGORIA É DE 5 CARACTERES")
 	private String descricao;
-
+	
+	
+	@OneToMany(fetch=FetchType.LAZY,mappedBy="categoria",cascade=CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
 	public Categoria() {
 		super();
 	}
 
-	public Categoria(Long id, String nome, String descricao) {
+	
+	public Categoria(Long id,
+			@NotBlank(message = "NOME DA CATEGORIA É OBRIGATÓRIO") @Size(min = 5, message = "O TAMANHO MINIMO DO NOME PARA A CATEGORIA É DE 5 CARACTERES") String nome,
+			@Size(min = 5, message = "O TAMANHO MINIMO DA DESCRICAO PARA A CATEGORIA É DE 5 CARACTERES") String descricao,
+			List<Produto> produto) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
+		this.produto = produto;
 	}
+
 
 	public Long getId() {
 		return id;
@@ -55,5 +72,15 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+
 
 }
